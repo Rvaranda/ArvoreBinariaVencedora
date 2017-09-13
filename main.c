f7577acb368e9e11f2ee56f26b1e45dae80f87de
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2017-09-13 15:53:07 -0300

    Commit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "client.h"
+#define MAX_OPEN_FILES 4
 
 
 typedef struct Node
@@ -17,34 +18,45 @@ typedef struct BinTree
     int totalNodes;
 } BinTree;
 
-BinTree* createWinnerBinTree(int numPart);
+BinTree* createWinnerBinTree(Node* n[], int numPart);
 
 int main()
 {
     FILE* partsToRead = fopen("filesToRead.txt", "r");
     if (partsToRead == NULL) return 1;
 
+    int i;
     int num = 0;
     char line[20];
 
-    while ((fscanf(partsToRead, "%s", line)) != EOF)
+    while ((fscanf(partsToRead, "%s", line)) != EOF && num <= MAX_OPEN_FILES) num++;
+    rewind(partsToRead);
+
+    Node* leafNodes[num];
+    FILE* files[num];
+
+    for (i = 0; i < num; i++)
     {
-        printf("%s\n", line);
-
-        FILE* test = fopen(line, "wb");
-        if (test != NULL) fclose(test);
-
-        num++;
+        fscanf(partsToRead, "%s", line);
+        files[i] = fopen(line, "rb");
+        if (files[i] == NULL)
+        {
+            printf("Error in opennig file %s\n", line);
+            return 1;
+        }
     }
 
-    printf("%d\n", num);
+    for (i = 0; i < num; i++)
+    {
+        fclose(files[i]);
+    }
 
     fclose(partsToRead);
 
     return 0;
 }
 
-BinTree* createWinnerBinTree(int numPart)
+BinTree* createWinnerBinTree(Node* n[], int numPart)
 {
     BinTree* t = (BinTree*)malloc(sizeof(BinTree));
     if (t != NULL)
